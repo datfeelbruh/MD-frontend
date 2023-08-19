fc23f90ff2dbeca013d7a1f218058d556a12394b
hash <maksim23090421@gmail.com>
2023-08-20 04:23:00 +0700

    Fix api wrapper error handling
    - now only status <= 200 & status > 300 will cause error

--- src/api/base/apiHook.ts
+++ src/api/base/apiHook.ts
@@ -20,7 +20,7 @@ export default function useApi<S, F, B = void>(request: ApiRequest<B>, onSuccess
       ...(request.body !== null && request.body !== undefined && { body: JSON.stringify(request.body) })
     })
       .then(data => data.json())
-      .then(data => data?.statusCode === undefined ? data : Promise.reject(new Error(JSON.stringify(data))))
+      .then(data => data?.statusCode === undefined || (data?.statusCode >= 200 && data?.statusCode < 300) ? data : Promise.reject(new Error(JSON.stringify(data))))
       .then(data => { setLoading(false); setResponse({ success: data }); onSuccess?.(data); })
       .catch(error => {
         setLoading(false);
