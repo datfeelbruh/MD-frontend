0c5dd9df9208055ab5e032583bb40ead04dec8b6
hash <maksim23090421@gmail.com>
2023-08-19 13:23:57 +0700

    Refactor user page
    - Replace extracting user id from token with getting it from a user
      store
    - Add loading page and error page
    - Refactor api calls to a new api wrapper

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -1,11 +1,12 @@
-import LoadingSvg from "@components/atomic/LoadingSpin";
+import useUserMoviesRequest from "@api/movie/userMoviesRequest";
 import Paginator from "@components/atomic/Paginator";
 import ExpandedReviewCard from "@components/reviewCards/ExpandedReviewCard";
 import ExpandedUserCard from "@components/userCards/ExpandedUserCard";
-import { tokenStore } from "@stores/tokenStore";
-import { displayError, get } from "@utils/requests";
-import { MOVIES_URL } from "@utils/urls";
-import { useState, useMemo, useEffect } from "preact/hooks";
+import Error from "@pages/Error";
+import Loading from "@pages/Loading";
+import { userStore } from "@stores/userStore";
+import { useEffect } from "preact/hooks";
+import { toast } from "react-toastify";
 
 
 interface UserProps {
@@ -13,30 +14,31 @@ interface UserProps {
 }
 
 export default function User({ id }: UserProps) {
-  const [user, setUser] = useState({ user: { username: "", avatar: null, about: "" }, movies: [], pages: 1, page: 1 });
-  const token = tokenStore(state => state.token);
+  const currentUserId = userStore(state => state?.user?.id);
 
-  const isCurrentUserPage = useMemo(() => {
-    return token !== null && JSON.parse(window.atob(token.split(".")[1])).userId == id;
-  }, [id])
+  const { call, response, setResponse, isLoading, isError } = useUserMoviesRequest(
+    id, { page: 1 }, () => { },
+    error => toast.error(error.message)
+  );
 
-  useEffect(() => {
-    get(`${MOVIES_URL.USER_MOVIES(id)}?user=${user.page}`)
-      .then(data => data.json())
-      .then(data => setUser(data))
-      .catch(error => displayError(error));
-  }, [user.page, id]);
+  const setPage = (page) => {
+    response.success.page = page;
+    setResponse({ ...response });
+  };
 
-  if (user.user.username === "") return <LoadingSvg />;
+  useEffect(() => call(), [response?.success?.page, id]);
+
+  if (isLoading) return <Loading />;
+  if (isError) return <Error message={response?.fail?.message} />;
 
   return (
     <div>
-      {isCurrentUserPage ? <a class="flex rounded bg-nord1 hover:bg-nord2 w-full p-1 mb-1 justify-center" href={`/userSettings/${id}`}>Редактировать профиль</a> : null}
-      <ExpandedUserCard {...user.user} />
+      {currentUserId === Number(id) ? <a class="flex rounded bg-nord1 hover:bg-nord2 w-full p-1 mb-1 justify-center" href={`/userSettings/${id}`}>Редактировать профиль</a> : null}
+      <ExpandedUserCard {...response.success.user} />
       <div class="mb-1" />
-      {user.pages > 1 ? <Paginator page={user.page} maxPage={user.pages} setPage={(page) => setUser({ ...user, page })} /> : null}
-      {user.movies.map(m => <ExpandedReviewCard {...m} />)}
-      {user.pages > 1 ? <Paginator page={user.page} maxPage={user.pages} setPage={(page) => setUser({ ...user, page })} /> : null}
+      {response.success.pages > 1 ? <Paginator page={response.success.page} maxPage={response.success.pages} setPage={setPage} /> : null}
+      {response.success.movies.map(m => <ExpandedReviewCard {...m} />)}
+      {response.success.pages > 1 ? <Paginator page={response.success.page} maxPage={response.success.pages} setPage={setPage} /> : null}
     </div>
   );
 }
