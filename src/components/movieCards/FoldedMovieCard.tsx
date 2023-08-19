f607ee32df2f0ad3b8973de8d32b3f74fd393a03
hash <maksim23090421@gmail.com>
2023-08-20 06:06:18 +0700

    Refactor home
    - Add footer with important links
    - Add popular movie list
    - Add api wrapper for popular movie request

---
+++ src/components/movieCards/FoldedMovieCard.tsx
@@ -0,0 +1,16 @@
+interface FoldedMovieCardProps {
+  reviewCount: number;
+  id: number;
+  title: string;
+  poster: string;
+}
+
+export default function FoldedMovieCard({reviewCount, id, title, poster}: FoldedMovieCardProps) {
+  return (
+    <a class="flex flex-col p-1 bg-nord-1 m-1 rounded w-1/5 hover:bg-nord-2 min-w-min" key={id} href={`/movie/${id}`}>
+      <img class="rounded shadow-sm" src={poster} />
+      <p class="min-w-min text-center">{title}</p>
+      <p class="text-center">Оценило: {reviewCount}</p>
+    </a>
+  );
+}
