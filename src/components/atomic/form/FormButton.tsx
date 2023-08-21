c0ff5066f93cc9f394034bb88b59d5663b7a016a
hash <maksim23090421@gmail.com>
2023-08-21 21:46:14 +0700

    Fix form components
    - Add extended props
    - Add required to a input component

--- src/components/atomic/form/FormButton.tsx
+++ src/components/atomic/form/FormButton.tsx
@@ -1,15 +1,16 @@
 import LoadingSvg from "../LoadingSpin";
 
-interface ButtonProps {
+interface ButtonProps extends React.ComponentProps<any> {
   text: string;
   isLoading: boolean;
   name?: string | undefined;
   type?: "submit" | "button" | "reset" | undefined;
 }
 
-export default function FormButton({ text, isLoading, name = undefined, type = "submit" }: ButtonProps) {
+export default function FormButton({ text, isLoading, name = undefined, type = "submit", ...props }: ButtonProps) {
   return (
     <button
+      {...props}
       class={`w-full flex flex-row justify-center m-auto p-0.5 rounded ${isLoading ? "bg-nord1" : "bg-nord2 hover:bg-nord3"}`}
       type={type}
       name={name}
--- src/components/atomic/form/FormInput.tsx
+++ src/components/atomic/form/FormInput.tsx
@@ -16,6 +16,7 @@ export default function FormInput({ onInput, value = undefined, autocomplete = t
       onInput={e => onInput(e.target as HTMLInputElement)}
       autoComplete={autocomplete === true ? placeholder : autocomplete as string | undefined}
       type={type}
+      required
     />
   );
 }
